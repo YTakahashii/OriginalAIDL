013d2653cfcab69ad50dfc5bc6ebbd678a5858c6
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2019-12-03 21:36:57 +0900

    feat: getMessage 呼び出し

--- OriginalAIDLClient/app/src/main/java/com/ytakahashi/originalaidlclient/MainActivity.kt
+++ OriginalAIDLClient/app/src/main/java/com/ytakahashi/originalaidlclient/MainActivity.kt
@@ -1,13 +1,60 @@
 package com.ytakahashi.originalaidlclient
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.IBinder
+import android.os.RemoteException
+import android.util.Log
+import android.widget.Toast
+import com.ytakahashi.IMyAidlInterface
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var svc: IMyAidlInterface? = null
+
+    private val connection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            svc = IMyAidlInterface.Stub.asInterface(service)
+            Toast.makeText(applicationContext, "Service Connected", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            svc = null
+            Toast.makeText(applicationContext, "Service Disconnected", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(svc == null) {
+            val it = Intent("originalservice")
+            it.setPackage("com.ytakahashi.originalaidlservice")
+            bindService(it, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            try {
+                val message = svc?.getMessage("yuta")
+                Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+                Log.d("client", e.toString())
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(connection)
     }
 
 }
