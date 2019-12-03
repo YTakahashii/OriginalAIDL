90e1a932c6ecb70d3c6859a3ccffffe8c82b3250
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2019-12-03 18:00:50 +0900

    Initial commit of Service

---
+++ app/src/main/java/com/ytakahashi/originalaidlservice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ytakahashi.originalaidlservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
